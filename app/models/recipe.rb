bf062d1b6a5493e301e3fd827df981df2f601db2
Sudarshan Khasnis <sudarshan.khasnis@gmail.com>
2017-08-25 18:10:42 +0530

    Add Key

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe
  include HTTParty
- ENV["FOOD2FORK_KEY"] = 'your api key from food2fork.com'
+ ENV["FOOD2FORK_KEY"] = '64b1d0b906e100db4a4a4efdbb8e5e3b'
  base_uri 'http://food2fork.com/api'
  default_params key: ENV["FOOD2FORK_KEY"], fields: "image_url,f2f_url,social_rank,title"
  format :json
